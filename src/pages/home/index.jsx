14f78b870e67487444893cacdaa45deb35fc9546
Youmna Jaza <youmnajaza@gmail.com>
2023-01-11 07:17:08 +0000

    Styled content     - Styled the info section

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -20,7 +20,7 @@ const Home = () => {
         width="100%"
         sx={{
           backgroundColor: palette.secondary.main,
-          p: "3rem",
+          p: "2rem",
         }}
       >
         <ul className="tools">
@@ -46,38 +46,52 @@ const Home = () => {
       </Box>
       {/* How */}
 
-      <Typography
-        variant="head"
+      <Box
         sx={{
-          color: palette.primary.dark,
-          fontWeight: "bold",
-          fontSize: "2.0rem",
-          wordWrap: "normal",
-          "&:hover": {
-            color: palette.primary.dark,
-          },
+          display: "flex",
+          alignItems: { md: "flex-end", xs: "center" },
+          flexDirection: { xs: "column", md: "row" },
+          justifyContent: { md: "space-between", xs: "center" },
+          p: "2rem 15%",
         }}
       >
-        How Fast box works
-      </Typography>
-      <Typography
-        variant="text"
-        sx={{
-          color: palette.text.secondary,
-          fontSize: { md: "1.5rem" },
-        }}
-      >
-        Duis aute irure dolor in reprehenderit in voluptate cillum dolore eu
-        fugiat nulla pariatur.
-      </Typography>
-      <img
-        className="star"
-        src={star}
-        alt="star"
-        sx={{
-          alignSelf: "flex-start",
-        }}
-      />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            gap: { md: "4rem", xs: "0" },
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            variant="head"
+            sx={{
+              color: palette.primary.dark,
+              fontWeight: "bold",
+              fontSize: { md: "2.0rem", xs: "1.25rem" },
+              "&:hover": {
+                color: palette.primary.dark,
+              },
+            }}
+          >
+            How Fast box works
+          </Typography>
+          <Typography
+            variant="text"
+            sx={{
+              width: { md: "40%", xs: "100%" },
+              color: palette.text.secondary,
+              fontSize: { md: "1.25rem" },
+              textAlign: { md: "left", xs: "center" },
+            }}
+          >
+            Duis aute irure dolor in reprehenderit in voluptate cillum dolore eu
+            fugiat nulla pariatur.
+          </Typography>
+        </Box>
+        <img src={star} alt="star" sx={{ alignSelf: "flex-end" }} />
+      </Box>
     </div>
   );
 };
